1e9bec6fa5f2ef90921de21a073949fc22f112ae
nada123ahmed <nada42928@gmail.com>
2025-07-31 05:21:55 +0300

    css modules changes

--- src/components/protectedRoutes/protectedRoutes.jsx
+++ src/components/protectedRoutes/protectedRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./ProtectedRoutes.module.css"
+import "./protectedRoutes.module.css"
 import { Navigate } from 'react-router-dom';
 export default function ProtectedRoutes(props) {
   if(localStorage.getItem("userToken")){
