231420653cefb3306f4449648252248e6c2a4f33
nada123ahmed <nada42928@gmail.com>
2025-07-31 04:55:09 +0300

    ecommerce-project

---
+++ src/components/ProtectedAuth/ProtectedAuth.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./ProtectedAuth.module.css"
+import { Navigate } from 'react-router-dom'
+export default function ProtectedAuth(props) {
+  if (localStorage.getItem("userToken")) {
+    
+    return <Navigate to="/"></Navigate>
+  }
+  else{
+    return props.children
+  }
+  
+  
+}
