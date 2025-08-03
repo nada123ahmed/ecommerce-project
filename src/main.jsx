6c05a1f6d44822e6a949689d9812b2c68f092b92
nada123ahmed <nada42928@gmail.com>
2025-08-03 21:55:44 +0300

    chsnges

--- src/main.jsx
+++ src/main.jsx
@@ -1,59 +1,30 @@
 
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import './index.css'
-// import App from './App.jsx'
-// import "@fortawesome/fontawesome-free/css/all.min.css"
-
-// import { TokenContextProvider } from './Context/TokenContext.jsx'
-// import CartContextProvider from './Context/CartContext.jsx';
-// import WishlistContextProvider from './Context/WishlistContext.jsx';
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <TokenContextProvider>
-//       <CartContextProvider>
-//         <WishlistContextProvider>
-        
-         
-
-//             <App />
-
-         
-//         </WishlistContextProvider>
-//       </CartContextProvider>
-//     </TokenContextProvider>
-//   </StrictMode>
-// )
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './index.css';
-import App from './App.jsx';
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import './index.css'
+import App from './App.jsx'
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
-// 1. قم باستيراد BrowserRouter
-import { BrowserRouter } from 'react-router-dom'; 
-
-import { TokenContextProvider } from './Context/TokenContext.jsx';
+import { TokenContextProvider } from './Context/TokenContext.jsx'
 import CartContextProvider from './Context/CartContext.jsx';
 import WishlistContextProvider from './Context/WishlistContext.jsx';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 2. اجعل BrowserRouter هو المكون الخارجي الذي يغلف كل شيء */}
-    <BrowserRouter> 
-      <TokenContextProvider>
-        <CartContextProvider>
-          <WishlistContextProvider>
+    <TokenContextProvider>
+      <CartContextProvider>
+        <WishlistContextProvider>
+        
+         
+
             <App />
-          </WishlistContextProvider>
-        </CartContextProvider>
-      </TokenContextProvider>
-    </BrowserRouter>
+
+         
+        </WishlistContextProvider>
+      </CartContextProvider>
+    </TokenContextProvider>
   </StrictMode>
-);
+)
